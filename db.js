a05aa92a3748596f508be1599d19f0f118c83f63
Theo Stavrides <theostavrides@gmail.com>
2020-04-07 23:42:40 -0700

    Send message to self when monitored users win or lose games

---
+++ db.js
@@ -0,0 +1,18 @@
+const redis = require("redis");
+const client = redis.createClient();
+const { promisify } = require("util");
+
+const setAsync = promisify(client.set).bind(client);
+const getAsync = promisify(client.get).bind(client);
+const delAsync = promisify(client.del).bind(client);
+
+module.exports = {
+	getUsersLastGameId: async user => {
+		return getAsync(user)
+	},
+
+	registerUsersLastGameId: async (user, game) => {
+		const gameid = lichess.getId(game)
+		return setAsync(user, gameid)
+	},
+}
--- index.js
+++ index.js
@@ -1,51 +1,78 @@
 const dotenv = require('dotenv'); dotenv.config();
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const lichess = require('./lib/chess/lichess')(process.env.LICHESS_TOKEN)
+const db = require('./db');
+const { PERSONAL_NUMBER } = process.env;
 
-const sendMessage = (to, msg) => {
-	return client.messages.create({
-		body: msg,
-		from: process.env.TWILIO_NUMBER,
-		to
-	})
-}
+const twilio = {
+	sendMessage: (to, msg) => {
+		return client.messages.create({
+			body: msg,
+			from: process.env.TWILIO_NUMBER,
+			to
+		})
+	}
+};
 
 const moveFrontToBack = arr => {
 	const copy = [...arr];
 	const front = copy.shift();
 	const newArray = [...copy, front];
 	return newArray;
-}
+};
 
 const lichessRequestHandler = async ({ tasks, throttle }) => {
 	const taskHandler = queue => {
 		const task = queue[0];
 		task()
 			.then(data => { 
-				const nextActions = moveFrontToBack(queue)
-				next(nextActions, throttle)
+				const nextActions = moveFrontToBack(queue);
+				next(nextActions, throttle);
 			})
 			.catch(err => { 
-				console.log(`Request failed with code ${err}, trying again in 61 seconds`)
-				next(queue, 61000) 
+				console.error(err);
+				console.log('Trying again in 61 seconds');
+				next(queue, 61000);
 			})
 	}
 	const next = (queue, ms) => setTimeout(() => taskHandler(queue), ms);
 	next(tasks, throttle);
-}
+};
 
-const logGameData = (user) => {
-	return () => lichess.getUserGames(user, { max: 1, parse: true }).then(games => {
-		const victor = lichess.getVictor(games[0])
-		console.log(victor)
-	})
-} 
+const handleNewGameDetected = (user, game, phoneNumber) => {
+	console.log(`new game detected for ${user}`)
+	const victor = lichess.getVictor(game);
+	const lost = victor !== user;
+	const message = lost ? 'lost' : 'won';
+	twilio.sendMessage(phoneNumber, `${user} has ${message} a new game.`);
+	db.registerUsersLastGameId(user, game);
+};
+
+const handleNoNewGameDetected = user => {
+	console.log(`No new game detected for ${user}`);
+};
+
+const sendMessageOnNewGame = (user, phoneNumber) => {
+	return async () => {
+		const savedGameId = await db.getUsersLastGameId(user);
+		if (savedGameId) {
+			const game = await lichess.getLastGameOfUser(user, { parse: true });
+			const lastLichessGameId = lichess.getId(game);
+			lastLichessGameId !== savedGameId ?
+				handleNewGameDetected(user, game, phoneNumber) :
+				handleNoNewGameDetected(user);
+		} else {
+			db.registerUsersLastGameId(user, game);
+		}
+	}
+}; 
 
 lichessRequestHandler({ 
 	throttle: 5000,
 	tasks: [
-		logGameData('dandyfap'),
-		logGameData('zvemoxes'),
-		logGameData('unclevarda')
+		sendMessageOnNewGame('dandyfap', PERSONAL_NUMBER),
+		sendMessageOnNewGame('zvemoxes', PERSONAL_NUMBER),
+		sendMessageOnNewGame('unclevarda', PERSONAL_NUMBER)
 	]
-})
+});
+
